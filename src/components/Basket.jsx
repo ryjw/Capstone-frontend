b4db495374a92326b56a7f538271e80a25a18580
Ryan Wilson <ryanjoelwilson@gmail.com>
2023-10-09 14:20:51 +0100

    basket logic added

--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -1,110 +1,111 @@
 import { NavLink, useOutletContext } from "react-router-dom";
 import { FiPlus, FiMinus } from "react-icons/fi";
+import { API } from "../API/api.js";
 
 export default function Basket() {
-  const { orderItems, items, order } = useOutletContext();
+  const { orderItems, items, order, fetchOpenOrder, token } =
+    useOutletContext();
+
+  function getItemInfo(id) {
+    const item = items.find((x) => id === x.id);
+    return item;
+  }
+
+  async function handleSubtract(orderItem) {
+    // e.preventDefault();
+    if (orderItem.quantity > 1) {
+      const res = await fetch(`${API}/orders/items/${orderItem.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          quantity: orderItem.quantity - 1,
+        }),
+      });
+      const info = await res.json();
+      if (info.success) {
+        fetchOpenOrder();
+      }
+    } else {
+      const res = await fetch(`${API}/orders/items/${orderItem.id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const info = await res.json();
+      if (info.success) {
+        fetchOpenOrder();
+      }
+    }
+  }
+  async function handleAdd(orderItem) {
+    // e.preventDefault();
+    const res = await fetch(`${API}/orders/items/${orderItem.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        quantity: orderItem.quantity + 1,
+      }),
+    });
+    const info = await res.json();
+    if (info.success) {
+      fetchOpenOrder();
+    }
+    fetchOpenOrder();
+  }
 
   return orderItems.length > 0 ? (
-    <div></div>
+    <div>
+      <h1>Basket</h1>
+      <div>
+        {orderItems.map((orderItem) => {
+          const item = getItemInfo(orderItem.menuItemId);
+          return (
+            <div key={orderItem.id}>
+              <h2>{item.name}</h2>
+              <p>${item.price.toFixed(2)}</p>
+              <p>
+                <FiMinus
+                  onClick={() => handleSubtract(orderItem)}
+                  className="plusminus"
+                />
+                <FiPlus
+                  onClick={() => handleAdd(orderItem)}
+                  className="plusminus"
+                />
+              </p>
+              <p>
+                X {orderItem.quantity} = $
+                {(item.price * orderItem.quantity).toFixed(2)}
+              </p>
+            </div>
+          );
+        })}
+        <div>
+          <h1>Basket total</h1>
+          <p>
+            $
+            {orderItems
+              .map((orderItem) => {
+                const item = getItemInfo(orderItem.menuItemId);
+                return orderItem.quantity * item.price;
+              })
+              .reduce((accumulator, currentValue) => accumulator + currentValue)
+              .toFixed(2)}
+          </p>
+        </div>
+      </div>
+    </div>
   ) : (
     <div>
       <h1>Basket empty</h1>
     </div>
   );
-
-  // fetch orderitems
-  // take orderItems and display the names in a list by filtering through items to match with the orderItemID//
-  // return (
-  //   <div>
-  //     <h1>Order</h1>
-  //     <ul>
-  //       {orderItems.map((orderItem) => {
-  //         return (
-  //           <li key={orderItem.id}>
-  //             {items
-  //               .filter((item) => item.id === orderItem.id)
-  //               .map((item) => {
-  //                 return (
-  //                   <div key={item.id}>
-  //                     {item.name} {item.price}
-  //                   </div>
-  //                 );
-  //               })}
-  //           </li>
-  //         );
-  //       })}
-  //     </ul>
-  //     <div>
-  //       Total price:{" "}
-  //       {orderItems
-  //         .map((item) => item.price)
-  //         .reduce((previous, current) => {
-  //           return previous + current;
-  //         })}
-  //     </div>
-  //     <div>
-  //       <NavLink to={"checkout"}>Check out</NavLink>
-  //     </div>
-  //   </div>
-  // );
 }
-
-/* export default function Basket() {
-  const { order, setOrder } = useOutletContext();
-  let uniqueItems = [];
-  for (let i = 0; i < order.length; i++) {
-    const id = order[i].id;
-    const found = uniqueItems.find((item) => item.id === id);
-    if (!found) {
-      uniqueItems.push(order[i]);
-    }
-  }
-
-  function handleAddItem(e, item) {
-    e.preventDefault();
-    setOrder([...order, item]);
-  }
-  function handleSubtractItem(e, item) {
-    e.preventDefault();
-    let newOrder = [...order];
-    const id = item.id;
-    const index = newOrder.findIndex((x) => x.id === id);
-    newOrder.splice(index, 1);
-    setOrder(newOrder);
-  }
-
-  return (
-    order.length > 1 && (
-      <div>
-        {uniqueItems.map((item) => {
-          return (
-            <div key={item.id}>
-              item: {item.name} quantity:
-              <FiMinus onClick={(e) => handleSubtractItem(e, item)} />
-              {order.filter((x) => x.id === item.id).length}
-              <FiPlus onClick={(e) => handleAddItem(e, item)} />
-              Price:
-              {order
-                .filter((x) => x.id === item.id)
-                .map((y) => y.price)
-                .reduce((previous, current) => {
-                  return previous + current;
-                })}
-            </div>
-          );
-        })}
-        <div>
-          Total price:{" "}
-          {order
-            .map((item) => item.price)
-            .reduce((previous, current) => {
-              return previous + current;
-            })}
-        </div>
-        <div>
-          <NavLink to={"checkout"}>Check out</NavLink>
-        </div>
-      </div>
-    )
-  );
-} */
