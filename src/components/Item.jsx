d3c7afee93d1716573f31b8e595adc5af773ae5f
romanjamess <118143164+romanjamess@users.noreply.github.com>
2023-10-09 14:42:27 -0500

    grid container on menu

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,14 +3,9 @@ import { NavLink } from "react-router-dom";
 export default function Item({ item, token, addToBasket }) {
   return (
     <NavLink to={`/item/${item.id}`}>
-      <div className="grid-container">
+      <div className="grid-item">
         <img className="item-image" src={`/Burger.jpg`} alt="food item" />
-        <div className="grid-item">
-          <h3>{item.name}</h3>
-        </div>
-        <div className="grid-item">
-          <h3>${item.price.toFixed(2)}</h3>
-        </div>
+        <h3>{item.name}</h3>
       </div>
     </NavLink>
   );
