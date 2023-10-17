df6d521ef620e895eb62fc22b44db5eeed96b3d7
romanjamess <118143164+romanjamess@users.noreply.github.com>
2023-10-17 12:21:13 -0500

    navbar logo and image

--- src/components/NavItems.jsx
+++ src/components/NavItems.jsx
@@ -17,6 +17,10 @@ function NavItems({ token, handleLogout, user }) {
       <nav className="navbar">
         <div className="navbar-container">
           <div className="logo-container">
+          {/* <img
+              className="logo"
+              src="/Rays.jpg"
+            ></img> */}
             <img
               className="logo"
               src="/Clam.png"
